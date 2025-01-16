59507b923fc1bc6bd3b04e868db3accce484b8ec
Mel⭐ <22073@portalcedi.edu.mx>
2025-01-16 09:54:05 -0600

    EA6 FINISHED COMMIT

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonInput, IonList, IonButton, IonIcon, IonCheckbox } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonInput, IonList, IonButton, IonIcon, IonCheckbox, FormControl]
 })
 export class ContactoPage implements OnInit {
+
+  nombre = new FormControl('');
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  funcionHola() {
+    alert("Todo OK");
   }
 
 }
